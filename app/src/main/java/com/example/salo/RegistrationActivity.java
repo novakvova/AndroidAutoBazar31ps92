b301b68761581b2d186873d13cbe1b76611c091b
freezpl <freezpl@gmail.com>
2020-03-14 22:11:38 +0200

    Registration form ready

--- app/src/main/java/com/example/salo/RegistrationActivity.java
+++ app/src/main/java/com/example/salo/RegistrationActivity.java
@@ -1,12 +1,17 @@
 package com.example.salo;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.example.salo.dto.RegisterDTO;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -23,5 +28,42 @@ public class RegistrationActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    public void SendRegistrationData(View view){
+        RegisterDTO data = new RegisterDTO();
+
+        EditText Login = (EditText) findViewById(R.id.txtLogin);
+        data.Login = Login.getText().toString().trim();
+
+        EditText Pass = (EditText) findViewById(R.id.txtPassword);
+        data.Password = Pass.getText().toString();
+
+        EditText Name = (EditText) findViewById(R.id.txtName);
+        data.Name = Name.getText().toString();
+
+        EditText Phone = (EditText) findViewById(R.id.txtPhone);
+        data.Phone = Phone.getText().toString();
+
+        if(data.Login.length() == 0 || data.Password.length() == 0)
+        {
+            AlertDialog.Builder dlgAlert = new AlertDialog.Builder(this);
+            String msg = "";
+            if(data.Login.length() == 0)
+                msg += "Login field is required!\n";
+            if(data.Password.length() == 0)
+                msg += "Password field is required!\n";
+            dlgAlert.setMessage(msg);
+            dlgAlert.setTitle("Registration error");
+            dlgAlert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) { }
+            });
+            dlgAlert.setCancelable(true);
+            dlgAlert.show();
+        }
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/salo/dto/RegisterDTO.java
@@ -0,0 +1,8 @@
+package com.example.salo.dto;
+
+public class RegisterDTO {
+    public String Login;
+    public String Password;
+    public String Name;
+    public String Phone;
+}
